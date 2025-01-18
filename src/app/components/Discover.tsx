2ae6e3fa90c09b697599576a34cecd7412d26abe
amitchackroborty04 <amitck803@gmail.com>
2025-01-18 11:58:38 +0600

    develop discovercard

--- src/app/components/Discover.tsx
+++ src/app/components/Discover.tsx
@@ -1,27 +1,50 @@
-import React from 'react'
-import Container from './Container'
-import Section_Heading from './Section_Heading'
-import Flex from './Flex'
-import DiscoverCard from './DiscoverCard'
+import React from "react";
+import Container from "./Container";
+import Section_Heading from "./Section_Heading";
+import Flex from "./Flex";
+import DiscoverCard from "./DiscoverCard";
 
 const Discover = () => {
   return (
     <section>
-        <Container>
-            <div className='flex justify-center mt-[74px]'>
-                <Section_Heading heading="Discover the world of Luxe Bloom" subheading="" />
-            </div>
-             <Flex>
-                <div className='w-[25%]'>
-                    <DiscoverCard/>
-                </div>
-                <div className='w-[25%]'></div>
-                <div className='w-[25%]'></div>
-                <div className='w-[25%]'></div>
-             </Flex>
-        </Container>
+      <Container>
+        <div className="flex justify-center mt-[74px]">
+          <Section_Heading
+            heading="Discover the world of Luxe Bloom"
+            subheading=""
+          />
+        </div>
+        <Flex className="mt-7">
+          <div className="w-[25%]">
+            {/* Passing a local image from the public directory */}
+            <DiscoverCard
+              imgsrc="/imge/hover_rose.jpg" // This is the correct path for a public folder image
+              title="Rose"
+            />
+          </div>
+          <div className="w-[25%]">
+            {/* Example of passing another local or remote image */}
+            <DiscoverCard
+                imgsrc="/imge/hover_rose.jpg" 
+              title="Lily"
+            />
+          </div>
+          <div className="w-[25%]">
+            <DiscoverCard
+               imgsrc="/imge/hover_rose.jpg" 
+              title="Tulip"
+            />
+          </div>
+          <div className="w-[25%]">
+            <DiscoverCard
+               imgsrc="/imge/hover_rose.jpg" 
+              title="Daisy"
+            />
+          </div>
+        </Flex>
+      </Container>
     </section>
-  )
-}
+  );
+};
 
-export default Discover
+export default Discover;
