8e126d8fcc768becf68a88cb1fce88ec4933586b
bdCalling <recantnafimahmed@gmail.com>
2025-01-18 12:55:17 +0600

    feat(hero): herosection style refactor

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,23 +1,15 @@
-import Image from 'next/image';
-import React from 'react';
-import heroimg from '@/../Public/imge/hero.jpg';
-
 const Hero = () => {
   return (
-    <section>
-      <div className=''>
-        <Image 
-          src={heroimg} 
-          alt="Hero Image" 
-          width={1920} // Adjust the original width
-          height={500} // Adjust the original height
-          layout="responsive" 
-          objectFit="cover" 
-          priority 
-          
-        />
-      </div>
-    </section>
+    <section className="h-screen bg-[url('https://dailyblooms.com.au/cdn/shop/files/All-blooms-01.jpg?v=1694341071&width=3008')] bg-cover bg-center">
+        <div className="container mx-auto px-4 h-full flex items-center">
+          <div className="mt-32">
+            <h1 className="text-6xl font-serif mb-6">VENUS DOESN&apos;T MISS.</h1>
+            <button className="bg-black text-white px-8 py-3 text-sm hover:bg-black/90 transition-colors">
+              SHOP VALENTINE&apos;S GIFTS
+            </button>
+          </div>
+        </div>
+      </section>
   );
 };
 
