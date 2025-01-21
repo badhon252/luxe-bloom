6fc7a41b930da3fca5ada6da0dcbfadacbd2360a
amitchackroborty04 <amitck803@gmail.com>
2025-01-21 12:50:02 +0600

    develop navdropdwon

---
+++ src/components/NavbarDropdwon/NavbarDropdwon.tsx
@@ -0,0 +1,96 @@
+import React from 'react';
+import Flex from '../Flex';
+import Image from 'next/image';
+import mini from "@/../public/assets/image/mini_rose (1).jpg"; // Ensure correct path and lowercase 'public'
+
+const NavbarDropdown = () => {
+  return (
+    <section className='  bg-white py-14'>
+      <div className="container ">
+        <Flex className='gap-x-16 justify-center '>
+          <div className="w-[110px] h-[110px]  ">
+          <Image
+              src={mini}
+              alt="Rounded image"
+              width={100}
+              height={100}
+              className="w-full h-full rounded-full object-cover "
+            />
+            <p className='text-[14px] text-[#000000]/70 font-normal leading-[14px] tracking-[1.4px] uppercase mt-3 text-center'>rose</p>
+          </div>
+          <div className="w-[110px] h-[110px]  ">
+          <Image
+              src={mini}
+              alt="Rounded image"
+              width={100}
+              height={100}
+              className="w-full h-full rounded-full object-cover "
+            />
+            <p className='text-[14px] text-[#000000]/70 font-normal leading-[14px] tracking-[1.4px] uppercase mt-3 text-center'>rose</p>
+
+          </div>
+          <div className="w-[110px] h-[110px]  ">
+          <Image
+              src={mini}
+              alt="Rounded image"
+              width={100}
+              height={100}
+              className="w-full h-full rounded-full object-cover "
+            />
+            <p className='text-[14px] text-[#000000]/70 font-normal leading-[14px] tracking-[1.4px] uppercase mt-3 text-center'>rose</p>
+
+          </div>
+          <div className="w-[110px] h-[110px]  ">
+          <Image
+              src={mini}
+              alt="Rounded image"
+              width={100}
+              height={100}
+              className="w-full h-full rounded-full object-cover "
+            />
+            <p className='text-[14px] text-[#000000]/70 font-normal leading-[14px] tracking-[1.4px] uppercase mt-3 text-center'>rose</p>
+
+          </div>
+        </Flex>
+        <div className='border border-[#000000]/50 w-[60%] mx-auto mt-12 '></div>
+        {/* ============list================= */}
+        <Flex className='gap-x-[276px] justify-center mt-7'>
+            <div>
+                <ul className='flex flex-col gap-y-5'>
+                    <li className='text-[12px] text-[#000000]/70 font-normal leading-[14px] uppercase'>all collections</li>
+                    <li className='text-[12px] text-[#000000]/70 font-normal leading-[14px] uppercase'>all collections</li>
+                    <li className='text-[12px] text-[#000000]/70 font-normal leading-[14px] uppercase'>all collections</li>
+                    <li className='text-[12px] text-[#000000]/70 font-normal leading-[14px] uppercase'>all collections</li>
+                </ul>
+            </div>
+            <div>
+            <ul className='flex flex-col gap-y-5'>
+                    <li className='text-[12px] text-[#000000]/70 font-normal leading-[14px] uppercase'>flowers by vessel</li>
+                    <li className='text-[12px] text-[#000000]/70 font-normal leading-[14px] uppercase'>all collections</li>
+                    <li className='text-[12px] text-[#000000]/70 font-normal leading-[14px] uppercase'>all collections</li>
+                    <li className='text-[12px] text-[#000000]/70 font-normal leading-[14px] uppercase'>all collections</li>
+                </ul>
+            </div>
+            <div>
+            <ul className='flex flex-col gap-y-5'>
+                    <li className='text-[12px] text-[#000000]/70 font-normal leading-[14px] uppercase'>bespoke collections</li>
+                    <li className='text-[12px] text-[#000000]/70 font-normal leading-[14px] uppercase'>all collections</li>
+                    <li className='text-[12px] text-[#000000]/70 font-normal leading-[14px] uppercase'>all collections</li>
+                    <li className='text-[12px] text-[#000000]/70 font-normal leading-[14px] uppercase'>all collections</li>
+                </ul>
+            </div>
+            <div>
+            <ul className='flex flex-col gap-y-5'>
+                    <li className='text-[12px] text-[#000000]/70 font-normal leading-[14px] uppercase'>by occasion</li>
+                    <li className='text-[12px] text-[#000000]/70 font-normal leading-[14px] uppercase'>all collections</li>
+                    <li className='text-[12px] text-[#000000]/70 font-normal leading-[14px] uppercase'>all collections</li>
+                    <li className='text-[12px] text-[#000000]/70 font-normal leading-[14px] uppercase'>all collections</li>
+                </ul>
+            </div>
+        </Flex>
+      </div>
+    </section>
+  );
+};
+
+export default NavbarDropdown;
