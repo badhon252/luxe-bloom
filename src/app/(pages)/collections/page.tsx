b46b9ad2274644d462a3f20c3ac0f2d01bb9bc61
amitchackroborty04 <amitck803@gmail.com>
2025-01-22 13:03:12 +0600

    develop collection-footer

--- src/app/(pages)/collections/page.tsx
+++ src/app/(pages)/collections/page.tsx
@@ -1,3 +1,5 @@
+import CollectionBulder from '@/components/Collections/CollectionBulder'
+import CollectionFooter from '@/components/Collections/CollectionFooter'
 import CollectionPage from '@/components/Collections/collections-page'
 import HeadingSection from '@/components/Collections/HeadingSection'
 import React from 'react'
@@ -7,7 +9,8 @@ const page = () => {
     <div className='container '>
         <HeadingSection/>
         <CollectionPage/>
-
+        <CollectionBulder/>
+        <CollectionFooter/>
     </div>
   )
 }
--- src/components/NavbarDropdwon/NavbarDropdwon.tsx
+++ src/components/NavbarDropdwon/NavbarDropdwon.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import Flex from '../Flex';
 import Image from 'next/image';
-import mini from "@/../../Public/assets/image/mini_rose.jpg"; // Ensure correct path and lowercase 'public'
+import mini from "@/../public/assets/image/mini_rose (1).png"; // Ensure correct path and lowercase 'public'
 
 const NavbarDropdown = () => {
   return (
-    <section className=' bg-white py-14'>
+    <section className='  bg-white py-14'>
       <div className="container ">
-        <div className='flex gap-x-16 justify-center '>
+        <Flex className='gap-x-16 justify-center '>
           <div className="w-[110px] h-[110px]  ">
           <Image
               src={mini}
@@ -50,10 +51,10 @@ const NavbarDropdown = () => {
             <p className='text-[14px] text-[#000000]/70 font-normal leading-[14px] tracking-[1.4px] uppercase mt-3 text-center'>rose</p>
 
           </div>
-        </div>
+        </Flex>
         <div className='border border-[#000000]/50 w-[60%] mx-auto mt-12 '></div>
         {/* ============list================= */}
-        <div className=' flex gap-x-[276px] justify-center mt-7'>
+        <Flex className='gap-x-[276px] justify-center mt-7'>
             <div>
                 <ul className='flex flex-col gap-y-5'>
                     <li className='text-[12px] text-[#000000]/70 font-normal leading-[14px] uppercase'>all collections</li>
@@ -86,7 +87,7 @@ const NavbarDropdown = () => {
                     <li className='text-[12px] text-[#000000]/70 font-normal leading-[14px] uppercase'>all collections</li>
                 </ul>
             </div>
-        </div>
+        </Flex>
       </div>
     </section>
   );
